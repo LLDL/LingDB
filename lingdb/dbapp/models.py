3679aed11aac41b0d537572ef73f5a0bf82787d8
adassor <adassor@sfu.ca>
2018-08-21 15:02:39 -0700

    Participation Relation added

--- lingdb/dbapp/models.py
+++ lingdb/dbapp/models.py
@@ -15,11 +15,73 @@ class Musical_Experience(models.Model):
         verbose_name_plural = "Musical Experiences"
     skill = models.CharField(max_length = 100)
 
-class Adult(models.Model):
-    sfu_id = models.IntegerField(blank = True, null = True)
+class Experiment(models.Model):
+    class Meta:
+        verbose_name = "Experiment"
+        verbose_name_plural = "Experiments"
+    experiment_name = models.CharField(max_length = 100)
+    LAB_CHOICES = (
+        ('ld', 'LangDev'),
+        ('ph', 'Phono'),
+    )
+    lab = models.CharField(
+        max_length = 2,
+        choices = LAB_CHOICES
+    )
+    STATUS_CHOICES = (
+        ('prep', 'In Prep'),
+        ('recruiting', 'Actively Recruiting'),
+        ('inactive', 'Inactive'),
+    )
+    status = models.CharField(
+        max_length = 10, 
+        choices = STATUS_CHOICES
+    )
+
+
+class Experiment_Section(models.Model):
+    class Meta:
+        verbose_name = "Experiment Section"
+        verbose_name_plural = "Experiment Sections"
+    experiment_section_name = models.CharField(max_length = 100)
+    STATUS_CHOICES = (
+        ('prep', 'In Prep'),
+        ('recruiting', 'Actively Recruiting'),
+        ('inactive', 'Inactive'),
+    )
+    status = models.CharField(
+        max_length = 10, 
+        choices = STATUS_CHOICES
+    )
+    section_of = models.ForeignKey(Language, on_delete=models.CASCADE)
+
+
+
+
+
+class Person(models.Model):
     given_name = models.CharField(max_length = 100)
     preferred_name = models.CharField(max_length = 100, blank = True, null = True)
-    surname = models.CharField(max_length = 100)
+    surname = models.CharField(max_length = 100, default='')
+    birth_date = models.DateField()
+    GENDER_CHOICES = (
+        ('F', 'Female'),
+        ('M', 'Male'),
+        ('O', 'Other'),
+    )
+    gender = models.CharField(
+        max_length = 1, 
+        choices = GENDER_CHOICES
+    )
+    def __str__(self):
+        return self.given_name
+
+    class Meta:
+        abstract = True
+    
+
+class Adult(Person):
+    sfu_id = models.IntegerField(blank = True, null = True)
     birth_date = models.DateField()
     address = models.CharField(max_length=200)
     years_of_education = models.SmallIntegerField(
@@ -30,16 +92,6 @@ class Adult(models.Model):
     )
     phone = models.CharField(max_length=50)
     email = models.EmailField()
-
-    GENDER_CHOICES = (
-        ('F', 'Female'),
-        ('M', 'Male'),
-        ('O', 'Other'),
-    )
-    gender = models.CharField(
-        max_length = 1, 
-        choices = GENDER_CHOICES
-    )
 
     CONTACT_CHOICES = (
         ('P', 'Phone'),
@@ -66,20 +118,16 @@ class Adult(models.Model):
         blank = True, 
         null = True
     )
-    def __str__(self):
-        return self.given_name
 
     languages = models.ManyToManyField(Language, through='Speaks')
     musical_experiences = models.ManyToManyField(Musical_Experience, through='IsExperiencedIn')
     parent_in = models.ManyToManyField(Family, through='IsParentIn', blank = True)
+    participations = models.ManyToManyField(Experiment_Section, through='Participated', blank = True)
 
 
-class Child(models.Model):
+class Child(Person):
     class Meta:
         verbose_name_plural = "Children"
-    given_name = models.CharField(max_length = 100)
-    preferred_name = models.CharField(max_length = 100, blank = True, null = True)
-    birth_date = models.DateField()
     gestation_length_weeks = models.SmallIntegerField(blank = True, null = True)
     was_full_term = models.BooleanField(blank = True, null = True)
     birth_weight = models.SmallIntegerField()
@@ -92,16 +140,7 @@ class Child(models.Model):
     health_notes = models.TextField(max_length=1000, blank = True, null = True)
     family = models.ManyToManyField(Family)
     exposed_to = models.ManyToManyField(Language, through='IsExposedTo')
-
-    GENDER_CHOICES = (
-        ('F', 'Female'),
-        ('M', 'Male'),
-        ('O', 'Other'),
-    )
-    gender = models.CharField(
-        max_length = 1, 
-        choices = GENDER_CHOICES
-    )
+    participations = models.ManyToManyField(Experiment_Section, through='Participated', blank = True)
 
 class IsExposedTo(models.Model):
     class Meta:
@@ -173,45 +212,13 @@ class IsParentIn(models.Model):
     family = models.ForeignKey(Family, on_delete = models.PROTECT)
     is_primary_contact = models.BooleanField()
 
-
-
-
-class Experiment(models.Model):
+class Participated(models.Model):
     class Meta:
-        verbose_name = "Experiment"
-        verbose_name_plural = "Experiments"
-    experiment_name = models.CharField(max_length = 100)
-    LAB_CHOICES = (
-        ('ld', 'LangDev'),
-        ('ph', 'Phono'),
-    )
-    lab = models.CharField(
-        max_length = 2,
-        choices = LAB_CHOICES
-    )
-    STATUS_CHOICES = (
-        ('prep', 'In Prep'),
-        ('recruiting', 'Actively Recruiting'),
-        ('inactive', 'Inactive'),
-    )
-    status = models.CharField(
-        max_length = 10, 
-        choices = STATUS_CHOICES
-    )
-
-
-class Experiment_Section(models.Model):
-    class Meta:
-        verbose_name = "Experiment Section"
-        verbose_name_plural = "Experiment Sections"
-    experiment_section_name = models.CharField(max_length = 100)
-    STATUS_CHOICES = (
-        ('prep', 'In Prep'),
-        ('recruiting', 'Actively Recruiting'),
-        ('inactive', 'Inactive'),
-    )
-    status = models.CharField(
-        max_length = 10, 
-        choices = STATUS_CHOICES
-    )
-    section_of = models.ForeignKey(Language, on_delete=models.CASCADE)
+        verbose_name = "Participation"
+        verbose_name_plural = "Participations"
+    adult_participant = models.ForeignKey(Adult, on_delete = models.PROTECT, blank=True)
+    child_participant = models.ForeignKey(Child, on_delete = models.PROTECT, blank=True)
+    section = models.ForeignKey(Experiment_Section, on_delete = models.PROTECT)
+    date = models.DateField()
+    notes = models.TextField(max_length=1000)
+    assessor = models.TextField(max_length=100)
