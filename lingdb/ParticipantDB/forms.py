c9f9f9b0bdeab5de2f66cea44c374b5078bd7633
adassor <adassor@sfu.ca>
2018-09-09 11:23:02 -0700

    birth_date functional as a datefield

--- lingdb/ParticipantDB/forms.py
+++ lingdb/ParticipantDB/forms.py
@@ -1,10 +1,13 @@
 from .models import Adult, Language, Musical_Skill
-from django.forms import ModelForm
+from django.forms import *
 
 class AdultForm(ModelForm):
     class Meta:
         model = Adult
         fields = ('id','given_name','preferred_name','surname','birth_date','gender','sfu_id','address','years_of_education','phone','email','contact_pref','pref_phone_time','health_notes','languages','musical_background')
+        widgets = {
+            'birth_date': DateInput(attrs={'type': 'date'})
+        }
 
 class LanguageForm(ModelForm):
     class Meta:
